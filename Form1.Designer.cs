a05460f6f7b310d347039d4007e0f69208f73bf1
akramessallak <117403353+akramessallak@users.noreply.github.com>
2022-11-27 18:26:23 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsListe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cbx = new System.Windows.Forms.ComboBox();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelPrenom = new System.Windows.Forms.Label();
+            this.TxtID = new System.Windows.Forms.TextBox();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtPrenom = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Cbx
+            // 
+            this.Cbx.FormattingEnabled = true;
+            this.Cbx.Location = new System.Drawing.Point(35, 36);
+            this.Cbx.Name = "Cbx";
+            this.Cbx.Size = new System.Drawing.Size(399, 21);
+            this.Cbx.TabIndex = 0;
+            this.Cbx.SelectedIndexChanged += new System.EventHandler(this.Cbx_SelectedIndexChanged);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(359, 213);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 48);
+            this.BtnCancel.TabIndex = 8;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(278, 213);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(75, 48);
+            this.BtnSave.TabIndex = 7;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(197, 213);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(75, 48);
+            this.BtnDelete.TabIndex = 6;
+            this.BtnDelete.Text = "Delete";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.Location = new System.Drawing.Point(116, 213);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(75, 48);
+            this.BtnUpdate.TabIndex = 5;
+            this.BtnUpdate.Text = "Update";
+            this.BtnUpdate.UseVisualStyleBackColor = true;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(35, 213);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(75, 48);
+            this.BtnAdd.TabIndex = 4;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(32, 72);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(18, 13);
+            this.labelID.TabIndex = 6;
+            this.labelID.Text = "ID";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(32, 109);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 7;
+            this.labelName.Text = "Name";
+            // 
+            // labelPrenom
+            // 
+            this.labelPrenom.AutoSize = true;
+            this.labelPrenom.Location = new System.Drawing.Point(32, 150);
+            this.labelPrenom.Name = "labelPrenom";
+            this.labelPrenom.Size = new System.Drawing.Size(43, 13);
+            this.labelPrenom.TabIndex = 8;
+            this.labelPrenom.Text = "Prenom";
+            // 
+            // TxtID
+            // 
+            this.TxtID.Location = new System.Drawing.Point(91, 65);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.Size = new System.Drawing.Size(343, 20);
+            this.TxtID.TabIndex = 1;
+            this.TxtID.TextChanged += new System.EventHandler(this.TxtID_TextChanged);
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(91, 102);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(343, 20);
+            this.TxtName.TabIndex = 2;
+            this.TxtName.TextChanged += new System.EventHandler(this.TxtName_TextChanged);
+            // 
+            // TxtPrenom
+            // 
+            this.TxtPrenom.Location = new System.Drawing.Point(91, 143);
+            this.TxtPrenom.Name = "TxtPrenom";
+            this.TxtPrenom.Size = new System.Drawing.Size(343, 20);
+            this.TxtPrenom.TabIndex = 3;
+            this.TxtPrenom.TextChanged += new System.EventHandler(this.TxtPrenom_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 287);
+            this.Controls.Add(this.TxtPrenom);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.TxtID);
+            this.Controls.Add(this.labelPrenom);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.Cbx);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Cbx;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelPrenom;
+        private System.Windows.Forms.TextBox TxtID;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox TxtPrenom;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsListe
+{
+    public partial class Form1 : Form
+    {
+        private bool Inup = false;
+        private bool Inadd = false;
+        private bool Indel = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string chaine = @"Data Source=DESKTOP-KIMOシ\SQLEXPRESS;Initial Catalog=dbListe;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        static SqlConnection cnx = new SqlConnection(chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initialstate();
+            display();
+        }
+        public void initialstate()
+        {
+            Cbx.Enabled = true;
+            TxtID.Enabled = false;
+            TxtName.Enabled = false;
+            TxtPrenom.Enabled = false;
+            BtnCancel.Enabled = false;
+            BtnSave.Enabled = false;
+            BtnUpdate.Enabled = false;
+            BtnDelete.Enabled = false;
+            BtnAdd.Enabled = true;
+        }
+        public void display()
+        {
+            Cbx.Items.Clear();
+            cmd.Connection = cnx;
+            cmd.CommandText = "select * from dbo.list";
+            if (cmd.Connection.State == ConnectionState.Open)
+            {
+                cmd.Connection.Close();
+            }
+            cnx.Open();
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                Cbx.Items.Add(dr["ID"].ToString());
+                Cbx.Items.Add(dr["Name"]);
+                Cbx.Items.Add(dr["Prenom"]);
+                Cbx.Items.Add("------------------------------");
+            }
+        }
+
+        private void Cbx_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtPrenom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            Inadd = true;
+
+            TxtID.Clear();
+            TxtName.Clear();
+            TxtPrenom.Clear();
+            display();
+            Cbx.Enabled = false;
+            TxtID.Enabled = true;
+            TxtName.Enabled = true;
+            TxtPrenom.Enabled = true;
+            BtnCancel.Enabled = true;
+            BtnSave.Enabled = true;
+            BtnUpdate.Enabled = false;
+            BtnDelete.Enabled = false;
+            BtnAdd.Enabled = false;
+
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            Inup = true;
+
+            Cbx.Enabled = true;
+            TxtID.Enabled = true;
+            TxtName.Enabled = true;
+            TxtPrenom.Enabled = true;
+            BtnCancel.Enabled = true;
+            BtnSave.Enabled = true;
+            BtnUpdate.Enabled = false;
+            BtnDelete.Enabled = false;
+            BtnAdd.Enabled = false;
+            display();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+
+            Indel = true;
+            string msgbox = "Are you sure you want to delete this item?";
+
+            string YNBox = "Confirm Deletion";
+
+
+            if (MessageBox.Show(msgbox, YNBox, MessageBoxButtons.YesNo) == DialogResult.No)
+
+            {
+                if (cmd.Connection.State == ConnectionState.Open)
+                {
+                    cmd.Connection.Close();
+                }
+                cnx.Open();
+                cmd.Connection = cnx;
+                cmd.CommandText = "delete from List where id='" + TxtID.Text + "' ";
+                cmd.ExecuteNonQuery();
+                cnx.Close();
+                TxtID.Clear();
+                TxtName.Clear();
+                TxtPrenom.Clear();
+                initialstate();
+            }
+            else
+            {
+                Cbx.Enabled = true;
+                TxtID.Enabled = false;
+                TxtName.Enabled = false;
+                TxtPrenom.Enabled = false;
+                BtnCancel.Enabled = false;
+                BtnSave.Enabled = false;
+                BtnUpdate.Enabled = true;
+                BtnDelete.Enabled = true;
+                BtnAdd.Enabled = true;
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            if (cmd.Connection.State == ConnectionState.Open)
+            {
+                cmd.Connection.Close();
+            }
+            cmd.Connection = cnx;
+            cnx.Open();
+
+
+            if (Inadd)
+            {
+                cmd.CommandText = "INSERT INTO List (ID,Name,Prenom ) " + "values('" + TxtID.Text + "','" + TxtName.Text + "','" + TxtPrenom.Text + "')";
+                cmd.ExecuteNonQuery();
+                Inadd = false;
+            }
+
+            if (Inup)
+            {
+                cmd.CommandText = "UPDATE List set Name='" + TxtName.Text + "',Prenom ='" + TxtPrenom.Text + "' where ID ='" + TxtID.Text + "'"; 
+                cmd.ExecuteNonQuery();
+                Inup = false;
+
+            }
+
+            cnx.Close();
+
+            Cbx.Enabled = true;
+            TxtID.Enabled = false;
+            TxtName.Enabled = false;
+            TxtPrenom.Enabled = false;
+            BtnCancel.Enabled = false;
+            BtnSave.Enabled = false;
+            BtnUpdate.Enabled = true;
+            BtnDelete.Enabled = true;
+            BtnAdd.Enabled = true;
+            display();
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            if (Inup)
+            {
+                Cbx.Enabled = true;
+                TxtID.Enabled = false;
+                TxtName.Enabled = false;
+                TxtPrenom.Enabled = false;
+                BtnCancel.Enabled = false;
+                BtnSave.Enabled = false;
+                BtnUpdate.Enabled = true;
+                BtnDelete.Enabled = true;
+                BtnAdd.Enabled = true;
+            }
+            else
+            {
+                initialstate();
+            }
+
+        }
+
+    }
+}
